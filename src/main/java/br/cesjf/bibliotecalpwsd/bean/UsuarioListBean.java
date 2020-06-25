a42bdd4195d4696e03135cbb2ac83581f4e5d542
Snyp <gabrielmarques.mg@gmail.com>
2020-06-25 19:44:02 -0300

    [Edit]_POM

--- src/main/java/br/cesjf/bibliotecalpwsd/bean/UsuarioListBean.java
+++ src/main/java/br/cesjf/bibliotecalpwsd/bean/UsuarioListBean.java
@@ -113,30 +113,4 @@ public class UsuarioListBean extends ProcessReport implements Serializable {
         }
     }
     
-    private String convertStringToMd5(String senha) throws UnsupportedEncodingException {
-   MessageDigest mDigest;
-   try { 
-     mDigest = MessageDigest.getInstance("MD5");
-             
-     byte[] valorMD5 = mDigest.digest(senha.getBytes("UTF-8"));
-      
-     StringBuffer sb = new StringBuffer();
-     for (byte b : valorMD5){
-            sb.append(Integer.toHexString((b & 0xFF) |
-            0x100).substring(1,3));
-     }
- 
-     return sb.toString();
-                 
-   } catch (NoSuchAlgorithmException e) {
-
-     e.printStackTrace();
-     return null;
-   } catch (UnsupportedEncodingException e) {
-     
-     e.printStackTrace();
-     return null;
-  }
-}
-    
 }
